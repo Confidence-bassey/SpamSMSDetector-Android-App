641cde52a9bd6d2aa318799c2abd3dfad73f35d8
Confidence-bassey <confidencebass2@gmail.com>
2023-10-14 00:16:59 +0100

    Added a Navigation drawer activty, navigtion header and our app's logo draWable

---
+++ app/src/main/java/com/confidenceb/spamsmsdetector/Nav_Drawer.java
@@ -0,0 +1,14 @@
+package com.confidenceb.spamsmsdetector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nav_Drawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_drawer);
+    }
+}
