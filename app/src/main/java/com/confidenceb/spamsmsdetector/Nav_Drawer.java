c6e3909aa84606783b148ce136a40a7a32f701df
Confidence-bassey <confidencebass2@gmail.com>
2023-10-22 23:59:44 +0100

    Implemented my Navigation drawer activity and added some fragments for the menu items

--- app/src/main/java/com/confidenceb/spamsmsdetector/Nav_Drawer.java
+++ app/src/main/java/com/confidenceb/spamsmsdetector/Nav_Drawer.java
@@ -1,14 +1,71 @@
 package com.confidenceb.spamsmsdetector;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
-public class Nav_Drawer extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Nav_Drawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_drawer);
+
+        Toolbar toolbar =(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_hamburg);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.open_drawer, R.string.Close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new com.confidenceb.spamsmsdetector.Messages()).commit();
+            navigationView.setCheckedItem(R.id.OpenMsgs);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed () {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        super.onBackPressed();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.Dashboard:
+                Log.i("INFO","ReadingSession called");
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new com.confidenceb.spamsmsdetector.Dashboard()).commit();
+                break;
+
+            case R.id.OpenMsgs:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new com.confidenceb.spamsmsdetector.Messages()).commit();
+                break;
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
