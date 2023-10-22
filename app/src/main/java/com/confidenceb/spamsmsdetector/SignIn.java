76b66252756c82751b85edd69e1b5e4a73f40cca
Confidence-bassey <confidencebass2@gmail.com>
2023-10-22 23:56:52 +0100

    Implemented Signup activity

--- app/src/main/java/com/confidenceb/spamsmsdetector/SignIn.java
+++ app/src/main/java/com/confidenceb/spamsmsdetector/SignIn.java
@@ -2,13 +2,26 @@ package com.confidenceb.spamsmsdetector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
+
+    Button signInbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        signInbtn = findViewById(R.id.btnLogin);
+        signInbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignIn.this, Nav_Drawer.class));
+            }
+        });
     }
 }
