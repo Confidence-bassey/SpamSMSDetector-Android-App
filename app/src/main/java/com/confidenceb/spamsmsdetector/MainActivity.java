6ac36d4b2e281b5c8db92f5c4146bb127c139f6c
Confidence-bassey <confidencebass2@gmail.com>
2023-10-22 23:54:52 +0100

    Added codes to implement my main activity

--- app/src/main/java/com/confidenceb/spamsmsdetector/MainActivity.java
+++ app/src/main/java/com/confidenceb/spamsmsdetector/MainActivity.java
@@ -2,13 +2,36 @@ package com.confidenceb.spamsmsdetector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signbtn, logbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signbtn = findViewById(R.id.createAccbtn);
+        logbtn = findViewById(R.id.btnLog);
+
+        signbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUp.class));
+            }
+        });
+
+        logbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignIn.class));
+            }
+        });
+
     }
 }
