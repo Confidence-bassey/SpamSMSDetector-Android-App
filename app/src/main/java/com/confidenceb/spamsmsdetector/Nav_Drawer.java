80f70e1fb2cc81ac37096fc6b1d53f397d6e7c23
Confidence-bassey <confidencebass2@gmail.com>
2023-11-03 04:17:04 +0100

    Added codes to implement the Navigation drawer functionality

--- app/src/main/java/com/confidenceb/spamsmsdetector/Nav_Drawer.java
+++ app/src/main/java/com/confidenceb/spamsmsdetector/Nav_Drawer.java
@@ -2,11 +2,14 @@ package com.confidenceb.spamsmsdetector;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -42,6 +45,34 @@ public class Nav_Drawer extends AppCompatActivity implements NavigationView.OnNa
 
     }
 
+    public void userLogOut(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(Nav_Drawer.this);
+        builder.setMessage("Do you want to log out?");
+        builder.setCancelable(true);
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finishAffinity();
+                System.exit(0);
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void shareApp() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        startActivity(intent);
+    }
+
     @Override
     public void onBackPressed () {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -56,12 +87,30 @@ public class Nav_Drawer extends AppCompatActivity implements NavigationView.OnNa
         switch (item.getItemId()) {
 
             case R.id.Dashboard:
-                Log.i("INFO","ReadingSession called");
+                Log.i("INFO","User dashboard");
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new com.confidenceb.spamsmsdetector.Dashboard()).commit();
                 break;
 
             case R.id.OpenMsgs:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new com.confidenceb.spamsmsdetector.Messages()).commit();
+                break;
+
+            case R.id.SpamMsgs:
+                Log.i("INFO","Spam");
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new com.confidenceb.spamsmsdetector.ArchiveMessages()).commit();
+                break;
+
+            case R.id.Notifications:
+                Log.i("INFO","Notification");
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new com.confidenceb.spamsmsdetector.Notifications()).commit();
+                break;
+
+            case R.id.ShareApp:
+                shareApp();
+                break;
+
+            case R.id.Logout:
+                userLogOut();
                 break;
 
         }
